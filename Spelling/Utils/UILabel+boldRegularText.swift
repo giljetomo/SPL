8dad99192a44466611bc2617dcfbad25cace1e39
Macbook Pro <gmjetomo@outlook.com>
2021-06-18 11:51:34 -0700

    feat: definitions in a collectionView

---
+++ Spelling/Utils/UILabel+boldRegularText.swift
@@ -0,0 +1,18 @@
+//
+//  UILabel+boldRegularText.swift
+//  Spelling
+//
+//  Created by Macbook Pro on 2021-06-18.
+//
+
+import UIKit
+
+extension UILabel {
+  public func setRegualAndBoldText(regualText: String, boldText: String) {
+      let attrs = [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: font.pointSize)]
+      let regularString = NSMutableAttributedString(string: regualText)
+      let boldString = NSMutableAttributedString(string: boldText, attributes:attrs)
+      boldString.append(regularString)
+      attributedText = boldString
+  }
+}
--- Spelling/ViewController.swift
+++ Spelling/ViewController.swift
@@ -5,25 +5,37 @@
 //  Created by Macbook Pro on 2021-06-14.
 //
 
-import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
   
-  var word: Word?
+  var word: Word? {
+    didSet {
+      guard let word = self.word else { return }
+      for (partOfSpeech, _) in word.definition {
+        self.partOfSpeech.append(partOfSpeech)
+      }
+    }
+  }
   var country: Country = .US
   var successfulFetch = true
+  var partOfSpeech = [String]()
   
-  let words = ["hello","asdfadsf","fabulous","mother","sdfsd","hero","sdss","example","handkerchief", "sir", "right", "hello", "obstreperous", "caa", "finish", "pair", "occur"]
+  let words = ["right","right","sir","pair","quixotic","right","above","apocryphal","sesquipedalian","hello","asdfadsf","fabulous","mother","sdfsd","hero","sdss","example","handkerchief", "sir", "right", "hello", "obstreperous", "caa", "finish", "pair", "occur"]
   var index = 0
   
   let topView: UIView = {
     let v = UIView()
     v.translatesAutoresizingMaskIntoConstraints = false
-    v.layer.cornerRadius = 4
-    v.backgroundColor = .lightGray
     return v
   }()
+  let definitionView: UIView = {
+    let v = UIView()
+    v.backgroundColor = .lightGray
+    v.translatesAutoresizingMaskIntoConstraints = false
+    return v
+  }()
+  var definitionCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
   
   var dictionViewIsOpen = true
   var dictionViewWidthConstraint: NSLayoutConstraint!
@@ -39,8 +51,8 @@ class ViewController: UIViewController {
     let sv = UIStackView(arrangedSubviews: [languageLabel, dictionUS, dictionUK])
     sv.translatesAutoresizingMaskIntoConstraints = false
     sv.axis = .horizontal
-    sv.alignment = .center
-    sv.distribution = .fillEqually
+    sv.alignment = .fill
+    sv.distribution = .equalSpacing
     sv.spacing = 5
     return sv
   }()
@@ -48,9 +60,24 @@ class ViewController: UIViewController {
     let lbl = UILabel()
     lbl.text = "US"
     lbl.font = .systemFont(ofSize: 16)
+    lbl.textAlignment = .center
+    lbl.layer.masksToBounds = true
+    lbl.isUserInteractionEnabled = true
+    lbl.setContentHuggingPriority(.required, for: .horizontal)
+    lbl.setContentCompressionResistancePriority(.required, for: .horizontal)
     return lbl
   }()
-  
+  let levelLabel: UILabel = {
+    let lbl = UIPaddedLabel(top: 5, bottom: 5, left: 5, right: 5)
+    lbl.translatesAutoresizingMaskIntoConstraints = false
+    lbl.text = Level.TRAVELLER.rawValue
+    lbl.font = .systemFont(ofSize: 16)
+    lbl.layer.masksToBounds = true
+    lbl.layer.cornerRadius = 5
+    lbl.backgroundColor = .white
+    lbl.textAlignment = .center
+    return lbl
+  }()
   let playButton: UIButton = {
     let btn = UIButton()
     btn.setTitle("Play", for: .normal)
@@ -86,32 +113,73 @@ class ViewController: UIViewController {
     btn.setTitle("Next", for: .normal)
     btn.translatesAutoresizingMaskIntoConstraints = false
     btn.setTitleColor(.systemBlue, for: .normal)
-    btn.addTarget(self, action: #selector(getNext(_:)), for: .touchUpInside)
+    btn.addTarget(self, action: #selector(fetchNextWord(_:)), for: .touchUpInside)
     return btn
   }()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = UIColor(named: "White")
+    
+    fetchWordAPI(with: country) {(successful) in
+      DispatchQueue.main.async {
+        self.definitionCollectionView.delegate = self
+        self.definitionCollectionView.dataSource = self
+        self.definitionCollectionView.collectionViewLayout = self.generateLayout()
+        self.changeButtonStateAfterFetch(successful)
+        UIView.animate(withDuration: 0.50) {
+          self.definitionCollectionView.reloadSections([0])
+        }
+      }
+    }
+    
+    definitionCollectionView.register(DefinitionCollectionViewCell.self, forCellWithReuseIdentifier: DefinitionCollectionViewCell.reuseIdentifier)
+    definitionCollectionView.backgroundColor = .white
     setupViewLayout()
     
     NotificationCenter.default.addObserver(self, selector: #selector(changeButtonStatePlayMode), name: .playbackEnded, object: nil)
-    
-    fetchWordAPI(with: country) {(successful)  in
-      self.changeButtonStateAfterFetch(successful)
+  }
+  
+  private func generateLayout() -> UICollectionViewLayout {
+    let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+      let item = NSCollectionLayoutItem(
+        layoutSize: NSCollectionLayoutSize(
+          widthDimension: .fractionalWidth(1.0),
+          heightDimension: .estimated(200)))
+      
+      let group = NSCollectionLayoutGroup.vertical(
+        layoutSize: NSCollectionLayoutSize(
+          widthDimension: .fractionalWidth(1.0),
+          heightDimension: item.layoutSize.heightDimension),
+        subitems: [item])
+      
+      let section = NSCollectionLayoutSection(group: group)
+      section.interGroupSpacing = 5
+      return section
     }
+    return layout
   }
   
   fileprivate func setupViewLayout() {
     view.addSubview(topView)
+    view.addSubview(definitionView)
     view.addSubview(playButton)
     view.addSubview(nextB)
-    
     
     topView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
     topView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.95).isActive = true
     topView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.05).isActive = true
     topView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    
+    definitionView.topAnchor.constraint(equalTo: topView.bottomAnchor, constant: 10).isActive = true
+    definitionView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.90).isActive = true
+    definitionView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.30).isActive = true
+    definitionView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    
+    definitionView.addSubview(definitionCollectionView)
+    definitionCollectionView.translatesAutoresizingMaskIntoConstraints = false
+    definitionCollectionView.widthAnchor.constraint(equalTo: definitionView.widthAnchor, multiplier: 1).isActive = true
+    definitionCollectionView.heightAnchor.constraint(equalTo: definitionView.heightAnchor, multiplier: 1).isActive = true
     
     topView.addSubview(dictionView)
     dictionViewWidthConstraint = NSLayoutConstraint(
@@ -123,15 +191,21 @@ class ViewController: UIViewController {
       multiplier: 0.10,
       constant: 0)
     topView.addConstraint(dictionViewWidthConstraint)
-    dictionView.addGestureRecognizer(dictionTapRecognizer)
+    languageLabel.addGestureRecognizer(dictionTapRecognizer)
+    
     dictionView.heightAnchor.constraint(equalTo: topView.heightAnchor, multiplier: 0.80).isActive = true
     dictionView.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: 8).isActive = true
     dictionView.centerYAnchor.constraint(equalTo: topView.centerYAnchor).isActive = true
     
     dictionView.addSubview(dictionVStackView)
-    dictionVStackView.leadingAnchor.constraint(equalTo: dictionView.leadingAnchor, constant: 5).isActive = true
+    dictionVStackView.widthAnchor.constraint(equalTo: dictionView.widthAnchor, multiplier: 0.90).isActive = true
+    dictionVStackView.heightAnchor.constraint(equalTo: dictionView.heightAnchor, multiplier: 0.90).isActive = true
     dictionVStackView.centerYAnchor.constraint(equalTo: dictionView.centerYAnchor).isActive = true
     dictionVStackView.centerXAnchor.constraint(equalTo: dictionView.centerXAnchor).isActive = true
+    
+    topView.addSubview(levelLabel)
+    levelLabel.trailingAnchor.constraint(equalTo: topView.trailingAnchor, constant: -8).isActive = true
+    levelLabel.centerYAnchor.constraint(equalTo: topView.centerYAnchor).isActive = true
     
     playButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
     playButton.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor).isActive = true
@@ -164,10 +238,14 @@ class ViewController: UIViewController {
     playButton.setTitleColor(playButton.isEnabled ? .systemBlue : .gray, for: .normal)
   }
   
-  @objc func getNext(_ sender: UIButton) {
+  @objc func fetchNextWord(_ sender: UIButton) {
     index += 1
-    fetchWordAPI(with: country) { (successful)  in
+    partOfSpeech.removeAll()
+    fetchWordAPI(with: country) { (successful) in
+      if !successful { self.word = nil }
       self.changeButtonStateAfterFetch(successful)
+      self.definitionCollectionView.reloadSections([0])
+      self.definitionCollectionView.scrollToItem(at: [0,0], at: .top, animated: false)
     }
   }
   
@@ -175,23 +253,31 @@ class ViewController: UIViewController {
     dictionViewIsOpen.toggle()
     
     let language = country == .US ? "US" : "UK"
+    dictionUK.backgroundColor = country == .US ? .clear : .cyan
+    dictionUS.backgroundColor = country == .UK ? .clear : .cyan
+    
     let multiplier = CGFloat(dictionViewIsOpen ? 0.10 : 0.30)
     topView.removeConstraint(dictionViewWidthConstraint)
     
-    UIView.animate(withDuration: 0.50) {
-      self.dictionUK.isHidden.toggle()
-      self.dictionUS.isHidden.toggle()
-      self.languageLabel.text = self.dictionViewIsOpen ? language : "✗"
-      self.dictionViewWidthConstraint = NSLayoutConstraint(
-        item: self.dictionView,
+    UIView.animate(withDuration: 0.25) { [weak self] in
+      self?.dictionUK.isHidden.toggle()
+      self?.dictionUS.isHidden.toggle()
+      self?.languageLabel.text = ""
+      self?.dictionViewWidthConstraint = NSLayoutConstraint(
+        item: self!.dictionView,
         attribute: .width,
         relatedBy: .equal,
-        toItem: self.topView,
+        toItem: self!.topView,
         attribute: .width,
         multiplier: multiplier,
         constant: 0)
-      self.topView.addConstraint(self.dictionViewWidthConstraint)
-      self.view.layoutIfNeeded()
+      self?.topView.addConstraint(self!.dictionViewWidthConstraint)
+      self?.view.layoutIfNeeded()
+    } completion: { (_) in
+      UIView.animate(withDuration: 0) { [weak self] in
+        self?.languageLabel.text = self!.dictionViewIsOpen ? language : "✖️" //🔙
+        self?.view.layoutIfNeeded()
+      }
     }
   }
   
@@ -203,7 +289,7 @@ class ViewController: UIViewController {
     }
     country.toggle()
     changeLanguage()
-    fetchWordAPI(with: country) { (successful)  in
+    fetchWordAPI(with: country) { (successful) in
       self.changeButtonStateAfterFetch(successful)
     }
   }
@@ -238,3 +324,80 @@ class ViewController: UIViewController {
   
 }
 
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    if let word = word {
+      return word.definition.count
+    }
+    return 1
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DefinitionCollectionViewCell.reuseIdentifier, for: indexPath) as! DefinitionCollectionViewCell
+    
+    UIView.animate(withDuration: 0.5, delay: 0.5 * Double(indexPath.item), usingSpringWithDamping: 1, initialSpringVelocity: 0.5, animations: {
+      AnimationUtility.viewSlideInFromTop(toBottom: cell)
+    })
+    
+    guard let word = word?.definition else {
+      cell.setup(with: "Error", and: "Please press next")
+      return cell
+    }
+    let partofSpeech = partOfSpeech[indexPath.item]
+    guard let definition = word[partofSpeech] else { return cell }
+    cell.setup(with: partofSpeech, and: definition)
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+    cell.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+    UIView.animate(withDuration: 0.25) { cell.transform = .identity }
+  }
+}
+
+// MARK: - AnimationUtility
+class AnimationUtility: UIViewController, CAAnimationDelegate {
+  
+  static let kSlideAnimationDuration: CFTimeInterval = 0.6
+  
+  static func viewSlideInFromRight(toLeft views: UIView) {
+    var transition: CATransition? = nil
+    transition = CATransition.init()
+    transition?.duration = kSlideAnimationDuration
+    transition?.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+    transition?.type = CATransitionType.push
+    transition?.subtype = CATransitionSubtype.fromRight
+    views.layer.add(transition!, forKey: nil)
+  }
+  
+  static func viewSlideInFromLeft(toRight views: UIView) {
+    var transition: CATransition? = nil
+    transition = CATransition.init()
+    transition?.duration = kSlideAnimationDuration
+    transition?.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+    transition?.type = CATransitionType.push
+    transition?.subtype = CATransitionSubtype.fromLeft
+    views.layer.add(transition!, forKey: nil)
+  }
+  
+  static func viewSlideInFromTop(toBottom views: UIView) {
+    var transition: CATransition? = nil
+    transition = CATransition.init()
+    transition?.duration = kSlideAnimationDuration
+    transition?.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+    transition?.type = CATransitionType.push
+    transition?.subtype = CATransitionSubtype.fromBottom
+    views.layer.add(transition!, forKey: nil)
+  }
+  
+  static func viewSlideInFromBottom(toTop views: UIView) {
+    var transition: CATransition? = nil
+    transition = CATransition.init()
+    transition?.duration = kSlideAnimationDuration
+    transition?.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+    transition?.type = CATransitionType.push
+    transition?.subtype = CATransitionSubtype.fromTop
+    views.layer.add(transition!, forKey: nil)
+  }
+}
